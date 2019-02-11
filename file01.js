14b699551a74e363d35aa69bd31c4991a1a7867f
Shubham Singal <singalshubham312@gmail.com>
2019-02-11 12:29:55 +0530

    nice to have you
    Signed-off-by: Shubham Singal <singalshubham312@gmail.com>

--- file01.js
+++ file01.js
@@ -2,5 +2,6 @@ function greet(name){
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
+    console.log('NIce to have you here !')
 }
 greet('shubham')
